ed8002111313fbc9293574767e0920e05b9283b4
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-09-19 23:28:58 +0300

    Delete ConditionalStatementsLab directory

--- ConditionalStatementsLab/_1_ExcellentResult.java
+++
@@ -1,7 +0,0 @@
-package ConditionalStatementsLab;
-import java.util.Scanner;
-public class _1_ExcellentResult {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int grade = Integer.parseInt(scanner.nextLine());
-        if(grade >= 5) {System.out.println("Excellent!");}}}
