61bb9aa5ce5a5444be6d283ce0c46f2c57e6e693
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-09-19 23:29:15 +0300

    Delete FirstStepsInCoding_Exercise directory

--- FirstStepsInCoding_Exercise/_1_USDtoBGN.java
+++
@@ -1,9 +0,0 @@
-package FirstStepsInCoding_Exercise;
-
-import java.util.Scanner;
-public class _1_USDtoBGN {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double usd = Double.parseDouble(scanner.nextLine());
-        double bgn = usd * 1.79549;
-        System.out.println(bgn);}}
--- FirstStepsInCoding_Exercise/_2_RadianstoDegrees.java
+++
@@ -1,8 +0,0 @@
-package FirstStepsInCoding_Exercise;
-import java.util.Scanner;
-public class _2_RadianstoDegrees {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double Radians = Double.parseDouble(scanner.nextLine());
-        double Degrees = Radians * 180 / Math.PI;
-        System.out.println(Degrees);}}
