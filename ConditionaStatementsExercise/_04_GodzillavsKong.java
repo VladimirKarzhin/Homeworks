362a87719c23d7f5dbd251354d6c183b4ae48160
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-09-19 23:28:34 +0300

    Delete ConditionaStatementsExercise directory

--- ConditionaStatementsExercise/_04_GodzillavsKong.java
+++
@@ -1,9 +0,0 @@
-package ConditionaStatementsExercise;
-
-import java.util.Scanner;
-
-public class _04_GodzillavsKong {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
