5e639fbb4b6a9c2d3e8f7d68573e0591246ae845
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-11-05 23:58:22 +0200

    Added CompanyUsers.js

---
+++ Fundamentals/AssociativeArraysExercise/_8_CompanyUsers.java
@@ -0,0 +1,37 @@
+package Fundamentals.AssociativeArraysExercise;
+
+import java.util.*;
+
+public class _8_CompanyUsers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, List<String>> companyEmployees = new LinkedHashMap<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("End")) {
+            String[] commandParts = input.split(" -> ");
+            String companyName = commandParts[0];
+            String employeeId = commandParts[1];
+
+            if (companyEmployees.containsKey(companyName)) {
+                List<String> currentEmployee = companyEmployees.get(companyName);
+                currentEmployee.add(employeeId);
+            } else {
+                companyEmployees.put(companyName, new ArrayList<>());
+                companyEmployees.get(companyName).add(employeeId);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        companyEmployees.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            //entry.getValue().forEach(employee -> System.out.println("-- " + employee));
+            for (String employee : entry.getValue()) {
+                System.out.println("-- " + employee);
+            }
+        });
+
+    }
+}
