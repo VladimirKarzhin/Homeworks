69ed7a9801f6d1da12ce663a7b36f95e2791adb1
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-11-09 20:52:53 +0200

    Update _5_DigitsLettersAndOther.java

--- Fundamentals/TextProcessingLab/_5_DigitsLettersAndOther.java
+++ Fundamentals/TextProcessingLab/_5_DigitsLettersAndOther.java
@@ -22,6 +22,7 @@ public class _5_DigitsLettersAndOther {
                 others.append(ch);
             }
         }
+        System.out.println(digits.toString());
         System.out.println(letters.toString());
         System.out.println(others.toString());
     }
