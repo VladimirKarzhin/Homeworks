7ef4c6897af3383fa182a1d5c479b1161aeb5270
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-11-11 22:57:11 +0200

    Added LettersChangeNumbers.js

---
+++ Fundamentals/TextProcessingExercise/_8_LettersChangeNumbers.java
@@ -0,0 +1,45 @@
+package Fundamentals.TextProcessingExercise;
+
+import java.util.Scanner;
+
+public class _8_LettersChangeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] codes = input.split("\\s+");
+
+        double sum = 0;
+        for (String code : codes) {
+            double modifiedNumber = getModifiedNumber(code);
+
+            sum += modifiedNumber;
+        }
+        System.out.printf("%.2f", sum);
+    }
+    private static double getModifiedNumber(String code) {
+        char letterBefore = code.charAt(0);
+        char letterAfter = code.charAt(code.length() - 1);
+        double number = Integer.parseInt(code.replace(letterBefore, ' ')
+                                             .replace(letterAfter, ' ')
+                                             .trim());
+
+        if (Character.isUpperCase(letterBefore)) {
+            int positionUpperLetter = (int) letterBefore - 64;
+            number /= positionUpperLetter;
+        } else {
+            int positionLowerLetter = (int) letterBefore - 96;
+            number *= positionLowerLetter;
+        }
+
+        if (Character.isUpperCase(letterAfter)) {
+            int positionUpperLetter = (int) letterAfter - 64;
+            number -= positionUpperLetter;
+        } else {
+            int positionLowerLetter = (int) letterAfter - 96;
+            number += positionLowerLetter;
+        }
+
+        return number;
+    }
+}
