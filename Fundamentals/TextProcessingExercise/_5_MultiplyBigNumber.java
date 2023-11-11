3282635d5a72bd0285a45a6ff7226b2096835f26
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-11-11 22:29:52 +0200

    Update _5_MultiplyBigNumber.java

--- Fundamentals/TextProcessingExercise/_5_MultiplyBigNumber.java
+++ Fundamentals/TextProcessingExercise/_5_MultiplyBigNumber.java
@@ -7,13 +7,32 @@ public class _5_MultiplyBigNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        BigInteger firstNumber = new BigInteger(scanner.nextLine());
-        BigInteger secondNumber = new BigInteger(scanner.nextLine());
+        //1. НАЧИН
+        //BigInteger firstNumber = new BigInteger(scanner.nextLine());
         //byte secondNumber = Byte.parseByte(scanner.nextLine());
-
-
         //System.out.println(firstNumber.multiply(new BigInteger(secondNumber + " ")));
-        System.out.println(firstNumber.multiply(secondNumber));
+
+        //2. НАЧИН
+        //BigInteger firstNumber = new BigInteger(scanner.nextLine());
+        //BigInteger secondNumber = new BigInteger(scanner.nextLine());
+        //System.out.println(firstNumber.multiply(secondNumber));
+
+        //3. НАЧИН
+        String firstNumber = scanner.nextLine();
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+
+        StringBuilder result = new StringBuilder();
+        int over = 0;
+
+        for (int position = firstNumber.length() - 1; position >= 0; position--) {
+            int digit = Integer.parseInt(firstNumber.charAt(position) + "");
+            int currentMultiply = digit * secondNumber + over;
+
+            over = currentMultiply / 10;
+            result.insert(0, currentMultiply % 10);
+        }
+        System.out.println(result);
+
 
     }
 }
