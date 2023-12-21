af34fc229989f1572281a18cdb826c4d5208a4e4
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2023-12-21 16:30:47 +0200

    Added 4, 5.js

---
+++ JavaAdvanced/StacksandQueuesLab/_5_PrinterQueue.java
@@ -0,0 +1,33 @@
+package JavaAdvanced.StacksandQueuesLab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _5_PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> printerQueue = new ArrayDeque<>();
+
+        String line = scanner.nextLine();
+
+        while (!line.equals("print")) {
+
+            if (line.equals("cancel")) {
+                if (printerQueue.size() == 0) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled" + " " + printerQueue.peek());
+                    printerQueue.pop();
+                }
+            } else {
+                printerQueue.offer(line);
+            }
+            line = scanner.nextLine();
+        }
+
+        while (!printerQueue.isEmpty()) {
+            System.out.println(printerQueue.pop());
+        }
+    }
+}
