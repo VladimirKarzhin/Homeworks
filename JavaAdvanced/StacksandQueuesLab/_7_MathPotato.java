15bdc25abd4eb6714fdeb8d45da7e08aedfe0f14
Vladimir Karzhin <145213385+VladimirKarzhin@users.noreply.github.com>
2024-01-10 12:29:46 +0200

    Added Exercise 6, 7, 8

---
+++ JavaAdvanced/StacksandQueuesLab/_7_MathPotato.java
@@ -0,0 +1,51 @@
+package JavaAdvanced.StacksandQueuesLab;
+
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class _7_MathPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        PriorityQueue<String> queue = new PriorityQueue<>();
+
+
+        String line = scanner.nextLine();
+        String[] arr = line.split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            String currentName = arr[i];
+            queue.offer(currentName);
+        }
+
+        int count = Integer.parseInt(scanner.nextLine());
+        int cycle = 1;
+
+        while (queue.size() != 1) {
+            for (int i = 1; i < count; i++) {
+                queue.offer(String.valueOf(queue.poll()));
+
+            }
+            if (isPrime(cycle)) {
+                System.out.println("Prime " + queue.peek());
+            } else {
+                System.out.println("Removed " + queue.poll());
+            }
+            cycle++;
+
+        }
+        System.out.println("Last is " + queue.poll());
+    }
+
+    private static boolean isPrime(int cycle) {
+            if (cycle <= 1)
+                return false;
+
+            for (int i = 2; i < cycle; i++)
+                if (cycle % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
